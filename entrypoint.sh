3957e517e3d4dfd60bd199518e20a67498759c77
Niek Palm <niek.palm@philips.com>
2020-03-24 11:00:27 +0100

    Add extraArgs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@
   --blackduck.trust.cert=true \
   --detect.policy.check=true \
   --detect.project.name=${INPUT_PROJECTNAME} \
-  --detect.project.version.name=${INPUT_VERSIONPREFIX}
+  --detect.project.version.name=${INPUT_VERSIONPREFIX} \
+  ${INPUT_EXTRAARGS}
